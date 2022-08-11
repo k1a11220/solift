e9ca039c041d29987e3fbccd06830e7eaead1ccf
Son Beomsoo <k1a11220@naver.com>
2022-08-11 10:47:57 +0900

    Create firebase press create objective log

---
+++ src/utils/firebaseAnalytics.tsx
@@ -0,0 +1,21 @@
+import * as Analytics from "expo-firebase-analytics";
+
+export const onPressCreateObjective = (uid: string, id: number) => {
+  Analytics.logEvent("onPressCreateObjective", {
+    uid: uid,
+    /*
+     * We want to know if the user came from from the swipe card as
+     * opposed to from chat or a deep link.
+     */
+    latestObjectiveId: id,
+    /*
+     * This may be too specific and not very useful, but maybe down the line * we could investigate why a certain user is more popular than others.
+     */
+    // user: uid,
+    /*
+     * We can use this information later to compare against other events.
+     */
+    screen: "profile",
+    purpose: "How many people are creating objectives?",
+  });
+};
